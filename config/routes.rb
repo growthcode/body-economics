3b99fc375361a6fde849f809567b9482b294cec3
Hethe Berg <growthcode@gmail.com>
2015-05-11 00:10:05 -0700

    update devise views with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get 'score_summary' => 'days#index', as: :score
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

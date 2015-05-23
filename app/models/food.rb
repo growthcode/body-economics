9eecdf06cb6fbbbc747574c676946281420e73f2
Hethe Berg <growthcode@gmail.com>
2015-05-22 18:56:08 -0700

    add uniquness of kind to food

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,5 +3,6 @@ class Food < ActiveRecord::Base
   has_many :meals, through: :servings
   has_many :tags, dependent: :destroy
   belongs_to :user
+  validates :kind, uniqueness: true
   validates :user_id, presence: true
 end
